741c9d76136224285a414962bb002b20a3d99ee3
Danis Nizamov <danis.nizamov@ivkhk.ee>
2026-03-04 13:29:33 +0200

    v3

--- front/server.js
+++ front/server.js
@@ -18,9 +18,23 @@ const config = {
 app.use(auth(config));
 
 app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() 
-    ? `Tere ${req.oidc.user.name}! <a href="/logout">Logout</a>` 
-    : 'Palun <a href="/login">Login</a>');
+  if (req.oidc.isAuthenticated()) {
+    res.send(`
+      <h1>Tere ${req.oidc.user.name}!</h1>
+      <a href="/logout">Logout</a>
+      <hr>
+      <p>payment button</p>
+      <!-- Stripe Buy Button -->
+      <script async src="https://js.stripe.com/v3/buy-button.js"></script>
+      <stripe-buy-button
+        buy-button-id="buy_btn_1T7DZwHHsHnJ2AMPJOFWf9Jl"
+        publishable-key="pk_test_51T7DBwHHsHnJ2AMPZSuAIjPx5cABFJHE283x5E3CTlDHjBiSXGa60QzEYdjHP6fu6xJBsVoKwfvUrWLxPOK8dboq00eAZgXzDQ"
+      >
+      </stripe-buy-button>
+    `);
+  } else {
+    res.send('Palun <a href="/login">Login</a>');
+  }
 });
 
 app.get('/health', (req, res) => {
@@ -31,10 +45,10 @@ app.get('/health', (req, res) => {
   });
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server tootab pordi ${PORT} peale`);
-});
-
 app.get('/payment-success', (req, res) => {
   res.send("Makse on edukas! 🎉 Aitäh tellimuse eest.");
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server tootab pordi ${PORT} peale`);
 });
