596b481ca2ee12f6f23851ef868c10e207bfa550
pupil <pupil@F245-LAB.net>
2026-03-19 09:44:13 +0200

    fuck 2.999

--- front/server.js
+++ front/server.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3100;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req,res) => {
     res.json({message: 'ok'})
