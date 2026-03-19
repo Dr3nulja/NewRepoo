b5f14658d9710ca350f25fe1d7be218103ca0062
pupil <pupil@F245-LAB.net>
2026-03-19 09:19:51 +0200

    fuck

--- front/src/services/billing.js
+++
@@ -1,9 +0,0 @@
-export function redirectToStripePaymentLink() {
-  const paymentLink = import.meta.env.VITE_STRIPE_PAYMENT_LINK;
-
-  if (!paymentLink) {
-    throw new Error('Missing VITE_STRIPE_PAYMENT_LINK in environment variables.');
-  }
-
-  window.location.assign(paymentLink);
-}
